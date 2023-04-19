619215b313fb0a85563f80320b0baa4faa5b84ed
donsonic9 <donsonic9@gmail.com>
2023-04-18 23:08:48 -0300

    addedCrossOrigin

--- BackEndDS9/src/main/java/com/portfolioYoProgramoAR/BackEndDS9/controller/HeroController.java
+++ BackEndDS9/src/main/java/com/portfolioYoProgramoAR/BackEndDS9/controller/HeroController.java
@@ -5,6 +5,7 @@ import com.portfolioYoProgramoAR.BackEndDS9.model.Hero;
 import com.portfolioYoProgramoAR.BackEndDS9.service.IHeroService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin (origins = "*")
 public class HeroController {
     
     @Autowired
