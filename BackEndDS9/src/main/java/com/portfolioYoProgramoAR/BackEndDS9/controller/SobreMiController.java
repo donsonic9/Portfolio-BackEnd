2cabbbba30aba798c9ad737e8f826388a18465a7
donsonic9 <donsonic9@gmail.com>
2023-04-23 23:55:19 -0300

    minorURLfixes

--- BackEndDS9/src/main/java/com/portfolioYoProgramoAR/BackEndDS9/controller/SobreMiController.java
+++ BackEndDS9/src/main/java/com/portfolioYoProgramoAR/BackEndDS9/controller/SobreMiController.java
@@ -5,6 +5,7 @@ import com.portfolioYoProgramoAR.BackEndDS9.model.SobreMi;
 import com.portfolioYoProgramoAR.BackEndDS9.service.ISobreMiService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin (origins = "http://localhost:4200/")
 public class SobreMiController {
     
     @Autowired
@@ -36,7 +38,7 @@ public class SobreMiController {
         sobremiServ.borrarSobreMi(id);
     }
     
-    @PutMapping ("/sobremi/editar")
+    @PutMapping ("/sobremi/editar/")
     public void editarSobreMi(@RequestBody SobreMi sobrem) {
         sobremiServ.editarSobreMi(sobrem);
     }
